0a0214b73e6795e15c94ee372c56bc807d26d819
Fodor Szilard <szityu@szityu.com>
2017-03-25 10:13:40 +0100

    Itinial commit => base Spring Boot application

---
+++ src/main/java/com/szityu/stackoverflow/MultiServletApplication.java
@@ -0,0 +1,12 @@
+package com.szityu.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiServletApplication.class, args);
+	}
+}
---
+++ src/test/java/com/szityu/stackoverflow/MultiServletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szityu.stackoverflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiServletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
